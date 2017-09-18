863f8345482ae1a3d055835eed69d863d05e841f
Ph03nix-BlackWings <ph03nix.mrv@gmail.com>
2017-09-18 21:30:32 +0700

    Create analystic.py

---
+++ Crypto/Another_Xor_100/analystic.py
@@ -0,0 +1,13 @@
+from libnum import *
+
+cipher = n2s(int('274c10121a0100495b502d551c557f0b0833585d1b27030b5228040d3753490a1c025415051525455118001911534a0052560a14594f0b1e490a010c4514411e070014615a181b02521b580305170002074b0a1a4c414d1f1d171d00151b1d0f480e491e0249010c150050115c505850434203421354424c1150430b5e094d144957080d4444254643',16))
+
+cipher = cipher[-32:]
+
+for i in range(0,len(cipher)):
+    print "Index %d" %i
+    for j in range(33,127):
+        tmp   = chr( j ^ ord(cipher[i]))
+        if( tmp in '1234567890abcdef'):
+            print chr(j),
+    print '\n' + '-' * 80
